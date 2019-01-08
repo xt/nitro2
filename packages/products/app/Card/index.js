7ffa2eb2acd35e0144e4d6743c4e702255e391c6
Mohd Imran <mohimran@WKMIN4269080.global.publicisgroupe.net>
2019-01-08 19:38:16 +0530

    Convert styles into emotion

--- packages/products/app/Card/index.js
+++ packages/products/app/Card/index.js
@@ -7,36 +7,70 @@ import { from } from "zen-observable";
 // import createCache from "@emotion/cache";
 
 const cardStyle = css`
-  border: thick solid #f00;
-  display: flex;
-  flex: 1;
-  box-sizing: border-box;
-  padding: 10px;
+max-width: 400px;
+background: #fff;
+padding: 0.5em;
+margin: 1em 0;
+border-radius: 4px;
+`;
+
+const buttonStyle = css`
+  background: #009999 url(http://i.imgur.com/RWKrr8S.png);
+  background-repeat: no-repeat;
+  background-position: 2em 50%;
+  background-size: 2em;
+  border: 0;
+  border-radius: 4px;
   cursor: pointer;
-  background: #eaeaea;
+  color: #FFF;
+  font-weight: bold;
+  font-size: 13px;
+  padding: 1em 3em;
+  padding-left: 6em;
+  text-transform: uppercase;
+  display: block;
+  width: 100%;
+  &:hover {
+    background-color: #006B6B;
+    background-size: 2.2em;
+    background-position: 1.5em 50%;
+  }
 `;
+const ulStyle = css`
+  list-style-type: none;
+  padding: 0;
+  margin-top:0;
+  display:flex;
+  justify-content: space-between;
+`;
+
+const liStyle = css`
+list-style: none;
+padding: 0.3em 0;
+border-radius: 1em;
+`;
+
+const imageStyle = css`
+
+`;
+
 const nameStyle = css`
-  font-family: san-serif;
-  font-size: 1rem;
-  color: #ff0066;
-`;
+  margin-bottom:0;
+`
+
 const Card = props => {
   const { product } = props;
   return (
-    <div className="card">
-   
-      <img src={product.picUrl} />
-       <h4 className="card-header--title">{product.productName}</h4>
+    <div css={cardStyle}>
+      <img css={imageStyle} src={product.picUrl} />
+       <h4 css={nameStyle}>{product.productName}</h4>
        <aside>
-        <ul>
-          <li>Price: Rs {product.price}</li>
+        <ul css={ulStyle}>
+          <li css={liStyle}>Price: Rs {product.price}</li>
           <li>In Stock</li>
         </ul>
-        <button>Add to Cart  {/* <LikeAction /> */}</button>
+        <button css={buttonStyle}>Add to Cart  {/* <LikeAction /> */}</button>
       </aside>
-    
-  
-   
     </div>
   );
 };
