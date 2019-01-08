a25737261685e782a9f4de363b896f462c688829
Vinci Rufus <vinci.rufus@gmail.com>
2019-01-08 22:18:03 +0530

    wip commit

---
+++ packages/products/functions/app/data/mock-data.js
@@ -0,0 +1,71 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var mockResponse = {
+  allProducts: [{
+    sku: "213213",
+    productName: "Classic Burgundy",
+    description: "Classic Burgundy Stretch Cotton Suit",
+    price: "39.56",
+    picUrl: "https://images.express.com/is/image/expressfashion/0039_03272124_0851_1_fb?cache=on&wid=361&fmt=jpeg&qlt=74,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon"
+  }, {
+    sku: "213213",
+    productName: "Charcoal Cotton Suit",
+    description: "Classic Charcoal Gray Oxford Cotton Suit",
+    price: "39.56",
+    picUrl: "https://images.express.com/is/image/expressfashion/0039_03271504_0904_2_fb?cache=on&wid=361&fmt=jpeg&qlt=74,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon"
+  }, {
+    sku: "213212",
+    productName: "Slim Plaid Windowpane",
+    description: "Extra Slim Gray Plaid Windowpane Wool-blend Stretch Suit",
+    price: "59.56",
+    picUrl: "https://images.express.com/is/image/expressfashion/0039_03252011_0904_3_fb?cache=on&wid=361&fmt=jpeg&qlt=74,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon"
+  }, {
+    sku: "213413",
+    productName: "Extra Slim Navy Suit",
+    description: "Extra Slim Navy Check Wool Suit",
+    price: "23.56",
+    picUrl: "https://images.express.com/is/image/expressfashion/0039_03252039_1378_4_fb?cache=on&wid=361&fmt=jpeg&qlt=74,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon"
+  }, {
+    sku: "12345",
+    productName: "Slim Plaid Suit",
+    description: "Slim Plaid Luxury 100% Italian Wool Suit",
+    price: "34.99",
+    picUrl: "https://images.express.com/is/image/expressfashion/0039_03272124_0851_1_fb?cache=on&wid=361&fmt=jpeg&qlt=74,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon"
+  }, {
+    sku: "15645",
+    productName: "Classic Black Tuxedo",
+    description: "Classic Black Cotton Sateen Tuxedo",
+    price: "44.99",
+    picUrl: "https://images.express.com/is/image/expressfashion/0039_03252011_0904_3_fb?cache=on&wid=361&fmt=jpeg&qlt=74,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon"
+  }, {
+    sku: "12345",
+    productName: "Mens Polo Shirt",
+    description: "lorem   lipsum",
+    picUrl: "https://images.express.com/is/image/expressfashion/0020_01763921_0098?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon",
+    price: "24.99"
+  }, {
+    sku: "216213",
+    productName: "Classic Burgundy",
+    description: "Classic Burgundy Stretch Cotton Suit",
+    price: "39.56",
+    picUrl: "https://images.express.com/is/image/expressfashion/0039_03272124_0851_1_fb?cache=on&wid=361&fmt=jpeg&qlt=74,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon"
+  }, {
+    sku: "253213",
+    productName: "Charcoal Cotton Suit",
+    description: "Classic Charcoal Gray Oxford Cotton Suit",
+    price: "39.56",
+    picUrl: "https://images.express.com/is/image/expressfashion/0039_03271504_0904_2_fb?cache=on&wid=361&fmt=jpeg&qlt=74,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon"
+  }, {
+    sku: "213412",
+    productName: "Slim Plaid Windowpane",
+    description: "Extra Slim Gray Plaid Windowpane Wool-blend Stretch Suit",
+    price: "59.56",
+    picUrl: "https://images.express.com/is/image/expressfashion/0039_03252011_0904_3_fb?cache=on&wid=361&fmt=jpeg&qlt=74,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon"
+  }]
+};
+var _default = mockResponse;
+exports.default = _default;
