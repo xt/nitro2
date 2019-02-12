278cc7f7c8c12bbc7d425a81bea00f738cd75fc6
Anand Kumar <5020381+kumar-anand@users.noreply.github.com>
2019-02-12 23:13:43 +0530

    updated plop to generate two types of template (js/ts) (#35)
    * updated plop to generate two types of template (js/ts)
    
    * package.json for template update

--- plopfile.js
+++ plopfile.js
@@ -1,47 +1,65 @@
-const templateFiles = "plop-template/**";
+const templateFilesJS = 'plop-templates/js/**';
+const templateFilesTS = 'plop-templates/ts/**';
 
-const defaultActions = [
+const jsTemplateActions = [
   {
-    type: "addMany",
-    destination: "packages/{{camelCase name}}",
-    base: "plop-template",
-    templateFiles
+    type: 'addMany',
+    destination: 'packages/{{camelCase name}}',
+    base: 'plop-templates/js',
+    templateFiles: templateFilesJS,
   },
   {
-    type: "modify",
-    path: "packages/{{camelCase name}}/package.json",
+    type: 'modify',
+    path: 'packages/{{camelCase name}}/package.json',
     pattern: /##__PROJECT_NAME_LOWER_CASE__##/gi,
-    template: "{{dashCase name}}"
-  }
+    template: '{{dashCase name}}',
+  },
+];
+const tsTemplateActions = [
+  {
+    type: 'addMany',
+    destination: 'packages/{{camelCase name}}',
+    base: 'plop-templates/ts',
+    templateFiles: templateFilesTS,
+  },
+  {
+    type: 'modify',
+    path: 'packages/{{camelCase name}}/package.json',
+    pattern: /##__PROJECT_NAME_LOWER_CASE__##/gi,
+    template: '{{dashCase name}}',
+  },
 ];
 
 module.exports = plop => {
   // Declare a new generator called "newApp" for use with our react-redux-boilerplate app
-  plop.setGenerator("bloks-core", {
-    description: "Scaffold a new React microApp",
+  plop.setGenerator('bloks-core', {
+    description: 'Scaffold a new React microApp',
 
     // Get inputs from the user.
     // That's Inquirer.js doing the job behind the hood.
     prompts: [
       {
-        type: "input",
-        name: "name",
-        message: "Name from your Micro App?",
+        type: 'input',
+        name: 'name',
+        message: 'Name from your Micro App?',
         validate: value => {
           if (/.+/.test(value)) {
             if (/^[a-zA-Z]+$/.test(value)) {
               return true;
             }
-            return "Name must not have any special characters in it.";
+            return 'Name must not have any special characters in it.';
           }
-          return "Name is required!";
-        }
-      }
+          return 'Name is required!';
+        },
+      },
+      {
+        type: 'confirm',
+        name: 'isJSTemplate',
+        message: 'Would you like to create typescript template?',
+      },
     ],
 
-    actions: function(answers) {
-      let actions = defaultActions;
-      return actions;
-    }
+    actions: answers =>
+      answers.isJSTemplate ? tsTemplateActions : jsTemplateActions,
   });
 };
