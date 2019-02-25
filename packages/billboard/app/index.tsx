3c64fb7f042d6e45ce2d067d1b6ea2da37f2a1e5
Mohd Imran <mohimran@WKMIN4269080.global.publicisgroupe.net>
2019-02-25 15:31:55 +0530

    Removed unused code

--- packages/billboard/app/index.tsx
+++ packages/billboard/app/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { hydrate } from "react-dom";
-import ItemList from "./ItemList";
 import mockResponse from "./data/mock-data.js";
 
 //const mockResponse = React.lazy(() => import('./data/mock-data'));
