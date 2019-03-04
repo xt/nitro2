b06754ae318f986592a97369be1ee715cda2c9df
Mohd Imran <mohimran@WKMIN4269080.global.publicisgroupe.net>
2019-03-04 22:32:59 +0530

    Disabed Turbolinks temporary

--- packages/vendors/index.js
+++ packages/vendors/index.js
@@ -1,5 +1,5 @@
-import turbolinksBootstrap from "./turbolinks/turbolinks-bootstrap";
-import pubSub from "./pubsub/pub-sub";
+//import turbolinksBootstrap from "./turbolinks/turbolinks-bootstrap";
+//import pubSub from "./pubsub/pub-sub";
 window.react = require("react");
 window.reactDOM = require("react-dom");
 window["@emotion/core"] = require("@emotion/core");
